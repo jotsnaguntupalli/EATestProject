b03e97848813d346b35715d9efabc8a8723f1db7
jotsna <djotsna@hotmail.com>
2016-06-03 08:48:22 +1000

    Includes all changes made to Login Page, Login Steps

--- EATestProject/Features/Login.feature
+++ EATestProject/Features/Login.feature
@@ -1,15 +1,15 @@
 ﻿Feature: Login
-	In order to login to the Hotmail homepage
+	In order to login to the EA homepage
 	As a registered user
 	I want to login to my account
 
 @Test
 @Browser:Firefox
-Scenario Outline: Login to Hotmail page
-	Given I have navigated to the Hotmail application
+Scenario Outline: Login to EA page
+	Given I have navigated to the EA application
 	And   I filled the <Username> and <Password> fields.
 	When  I press login button
-	Then  I should see the Hotmail homepage
+	Then  I should see the EA homepage
 
 Examples:
  | Username | Password |
--- EATestProject/Steps/LoginSteps.cs
+++ EATestProject/Steps/LoginSteps.cs
@@ -1,9 +1,12 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Configuration;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using OpenQA.Selenium;
+using OpenQA.Selenium.Firefox;
+using OpenQA.Selenium.Remote;
 using TechTalk.SpecFlow;
 
 namespace EATestProject.Steps
@@ -11,7 +14,14 @@ namespace EATestProject.Steps
     [Binding]
     public class LoginSteps
     {
-      
+        [Given(@"I have navigated to the EA application")]
+        public void IHaveNavigatedToEAApplication()
+        {
+            Driver.driver = new FirefoxDriver();
+            Driver.driver.Navigate().GoToUrl(ConfigurationManager.AppSettings["seleniumBaseUrl"]);
+            Driver.driver.Manage().Window.Maximize();
+        }
+        [When((@""))]
 
     }
 }
